0ba830285a7b8bb00995855b2666da016e441f32
Aimee Tacchi <aimee.tacchi@space01.co.uk>
2022-10-09 22:18:37 +0100

    removing link

--- src/components/nav/index.jsx
+++ src/components/nav/index.jsx
@@ -1,14 +1,9 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
 import NavStyles from './styles'
 
 const Nav = ({setPage}) => {
   return (
     <NavStyles>
       <ul className='nav-list'>
-        {/* <li className='nav-list__item'>
-          <Link to='/'>Home</Link>
-        </li> */}
          <li className='nav-list__item'>
           <button onClick={() => setPage('planets')}>Planets</button>
         </li>
